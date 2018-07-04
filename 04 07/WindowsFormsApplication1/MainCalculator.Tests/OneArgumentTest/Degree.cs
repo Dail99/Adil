8deff47b6a6facbb0b8a4a50b4179267f991a1be
vaif94 <acc_for_battle_net@mail.ru>
2018-07-04 11:53:33 +0600

    TestArguments

---
+++ 04 07/WindowsFormsApplication1/MainCalculator.Tests/OneArgumentTest/Degree.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+namespace MainCalculator.Tests.OneArgumentTest
+{
+    [TestFixture]
+    public class MultiTests
+    {
+        [Test]
+        public void CalculateMultiTest()
+        {
+            IOneArgumentCalculator calculator = new Radical();
+            double result = calculator.Calculate(16);
+            Assert.AreEqual(4, result);
+        }
+    }
+
+}
---
+++ 04 07/WindowsFormsApplication1/MainCalculator.Tests/TwoArgumentTest/multiTest.cs
@@ -0,0 +1,18 @@
+﻿
+using NUnit.Framework;
+
+namespace MainCalculator.Tests
+{
+    [TestFixture]
+    public class MultiTests
+    {
+        [Test]
+        public void CalculateMultiTest()
+        {
+            ITwoArgumentCalculator calculator = new Multi();
+            double result = calculator.Calculate(5, 10);
+            Assert.AreEqual(50, result);
+        }
+    }
+
+}
