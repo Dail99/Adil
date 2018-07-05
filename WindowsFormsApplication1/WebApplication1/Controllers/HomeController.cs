f39232228560f093db3d23806486c7b6257c37c0
Dail99 <naruto_006@Mail.ru>
2018-07-05 21:06:39 +0600

    Web ready

---
+++ WindowsFormsApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Web.Mvc;
+using MainCalculator.TwoArgument.TwoTwoArgumentsFactory;    
+using SelectListItem = System.Web.WebPages.Html.SelectListItem;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Operation = new SelectListItem[]
+            {
+                new SelectListItem()
+                {
+                    Value = "Multi", Text ="Multi"
+                },
+                new SelectListItem()
+                {
+                    Value = "Plus", Text ="Plus"
+                },
+                new SelectListItem()
+                {
+                    Value = "Minus", Text ="Minus"
+                },
+                new SelectListItem()
+                {
+                    Value = "Div", Text ="Div"
+                }
+            };
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(double firstValue, double secondValue, string operation)
+        {
+            ITwoArgumentCalculator calculator = TwoTwoArgumentsFactory.CreateCalculator(operation);
+            double result = calculator.Calculate(firstValue, secondValue);
+            ViewBag.Result = result;
+            ViewBag.Operation = new SelectListItem[]
+            {
+                new SelectListItem()
+                {
+                    Value = "Multi", Text ="Multi"
+                },
+                new SelectListItem()
+                {
+                    Value = "Plus", Text ="Plus"
+                },
+                new SelectListItem()
+                {
+                    Value = "Minus", Text ="Minus"
+                },
+                new SelectListItem()
+                {
+                    Value = "Div", Text ="Div"
+                }
+            };
+            return View();
+        }
+    }
+}
--- WindowsFormsApplication1/WindowsFormsApplication1/Properties/Resources.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MainCalculator.Properties {
+namespace WindowsFormsApplication1.Properties {
     using System;
     
     
